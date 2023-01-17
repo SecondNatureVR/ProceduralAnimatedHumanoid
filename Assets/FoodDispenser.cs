52e106c92d548b98aee5d578606b7386b0192bad
Johnny <secondnaturevr@gmail.com>
2023-01-16 23:17:06 -0500

    Food dispenser with signage

--- Assets/FoodDispenser.cs
+++ Assets/FoodDispenser.cs
@@ -4,13 +4,15 @@ using UnityEngine.InputSystem;
 public class FoodDispenser : MonoBehaviour
 {
     [SerializeField] public GameObject foodPrefab;
+    [SerializeField] public Transform player;
 
     public void Dispense()
     {
-        var food = Instantiate(foodPrefab, transform.position + transform.forward, transform.rotation);
+        Vector3 awayPlayer = Vector3.ProjectOnPlane((transform.position - player.position).normalized, Vector3.up);
+        var food = Instantiate(foodPrefab, transform.position + awayPlayer, transform.rotation);
         var rb = food.GetComponent<Rigidbody>();
-        rb.velocity = 8.0f * (Quaternion.AngleAxis(Random.Range(-90, 90), Vector3.up) * transform.forward + Vector3.up);
-        rb.angularVelocity = rb.transform.up * -10;
+        rb.velocity = 12.0f * (Vector3.Slerp(awayPlayer, Random.onUnitSphere, 0.1f) + (0.5f * Vector3.up)); ;
+        rb.angularVelocity = Random.onUnitSphere * 25.0f;
     }
 
     public void OnFire(InputAction.CallbackContext context)
--- Assets/RotateCone.cs
+++ Assets/RotateCone.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class RotateCone : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] public Transform player;
+
+    /// <summary> isolate the y-Component of a rotation </summary>
+    private Quaternion yRotation(Quaternion q)
     {
-        
+        float theta = Mathf.Atan2(q.y, q.w);
+
+        // quaternion representing rotation about the y axis
+        return new Quaternion(0, Mathf.Sin(theta), 0, Mathf.Cos(theta));
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Vector3 awayDir = transform.position - player.position;
+        Quaternion awayRotation = Quaternion.LookRotation(awayDir);
+        transform.rotation = yRotation(awayRotation);
     }
 }
